51b67aec5938dbdd49f5833fb0fcee53653b5212
Juliana Kang <julianajlk@gmail.com>
2018-06-02 01:10:02 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,15 @@
 const codes = [
-  38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
 
 let index = 0;
 
